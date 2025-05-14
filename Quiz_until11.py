03f2e7791a0e182d19335a4641f31d69ec65f5f0
Josocool <rouangchalern.phasouk11@gmail.com>
2025-05-14 16:40:52 +0700

    Quiz unitl 11

---
+++ Quiz_until11.py
@@ -0,0 +1,13 @@
+fruits_dic = {'apple': 6000, 'melon': 3000, 'banana': 5000, 'orange': 4000}
+
+print(fruits_dic.keys())
+
+if 'apple' in fruits_dic:
+    print("apple is in fruits_dic.")
+else:
+    print("apple is not in fruits_dic.")
+
+if 'mango' in fruits_dic:
+    print("mango is in fruits_dic.")
+else:
+    print("mango is not in fruits_dic.")
